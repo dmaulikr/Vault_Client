e96dd4dea9fa0ab15af9cba9c4ab94c01b48aece
Harrison Ferrone <hferrone@gmail.com>
2015-02-24 17:06:54 -0600

    First Commit

---
+++ Vault_ClientFacingApp/Vault_ClientFacingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vault_ClientFacingApp
+//
+//  Created by Harrison Ferrone on 2/24/15.
+//  Copyright (c) 2015 Harrison Ferrone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vault_ClientFacingApp/Vault_ClientFacingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vault_ClientFacingApp
+//
+//  Created by Harrison Ferrone on 2/24/15.
+//  Copyright (c) 2015 Harrison Ferrone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
