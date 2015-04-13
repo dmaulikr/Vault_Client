3a24d49f96da6c9bb0cd785a08f7009f42ad9791
Harrison Ferrone <hferrone@gmail.com>
2015-04-13 15:36:22 -0500

    Setup for post request from rails database

--- Vault_ClientFacingApp/Vault_ClientFacingApp/LoginViewController.m
+++ Vault_ClientFacingApp/Vault_ClientFacingApp/LoginViewController.m
@@ -8,7 +8,7 @@
 
 #import "LoginViewController.h"
 
-@interface LoginViewController ()
+@interface LoginViewController () <NSURLConnectionDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *loginLogoImage;
 @property (weak, nonatomic) IBOutlet UITextField *loginUsernameTextField;
@@ -16,6 +16,9 @@
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UIButton *loginRegisterButton;
 @property (weak, nonatomic) IBOutlet UILabel *loginMotoLabel;
+
+@property NSMutableData *responseData;
+@property NSDictionary *loginCredentials;
 
 @property UIColor *customGrey;
 
@@ -27,22 +30,96 @@
 {
     [super viewDidLoad];
 
-#pragma mark - Convenience Stuff
+//Convenience Stuff
     self.customGrey = [UIColor colorWithRed:(34/255.0) green:(34/255.0) blue:(34/255.0) alpha:1.0];
     
-#pragma mark - UI
+//UI
     self.view.backgroundColor = self.customGrey;
     self.loginUsernameTextField.backgroundColor = self.customGrey;
     self.loginPasswordTextField.backgroundColor = self.customGrey;
 
-#pragma mark - Instantiate Auto Layout
     [self addAutoLayoutConstraints];
 }
 
-#pragma mark - Status Bar Appereance
 -(BOOL)prefersStatusBarHidden
 {
     return YES;
+}
+
+#pragma mark - Login Methods
+- (IBAction)loginOnButtonPressed:(UIButton *)sender
+{
+    [self createUserInfoDictionary];
+}
+
+-(void)createUserInfoDictionary
+{
+    NSString *username = self.loginUsernameTextField.text;
+    NSString *password = self.loginPasswordTextField.text;
+    
+    self.loginCredentials = [[NSDictionary alloc]initWithObjectsAndKeys:@"username", username, @"password", password, nil];
+    
+    [self checkWithServer:self.loginCredentials];
+}
+
+#pragma mark - URL Post Request
+-(void)checkWithServer: (NSDictionary*)userCredentials
+{
+    NSData *jsonData;
+    NSError *error;
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@""]];
+    request.HTTPMethod = @"POST";
+    [request setValue:@"application/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    jsonData = [NSJSONSerialization dataWithJSONObject:userCredentials options:NSJSONWritingPrettyPrinted error:&error];
+    request.HTTPBody = jsonData;
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:NSOperationQueuePriorityNormal completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+        [self performSegueWithIdentifier:@"" sender:self];
+        
+        if (connectionError)
+        {
+            UIAlertView *loginFailed = [[UIAlertView alloc] initWithTitle:@"Stop!" message:@"Your username and/or password is incorrect, please re-enter." delegate:self cancelButtonTitle:@"Dimiss" otherButtonTitles:nil];
+            [loginFailed show];
+        }
+    }];
+}
+
+#pragma mark - URL Connection Delegate Methods
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    _responseData = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    // Append the new data to the instance variable you declared
+    [_responseData appendData:data];
+}
+
+- (NSCachedURLResponse *)connection:(NSURLConnection *)connection
+                  willCacheResponse:(NSCachedURLResponse*)cachedResponse {
+    // Return nil to indicate not necessary to store a cached response for this connection
+    return nil;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    // The request is complete and data has been received
+    // You can parse the stuff in your instance variable now
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    // The request has failed for some reason!
+    // Check the error var
+}
+
+#pragma mark - Resign Responders
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.loginUsernameTextField resignFirstResponder];
+    [self.loginPasswordTextField resignFirstResponder];
 }
 
 #pragma mark - Auto Layout
@@ -59,7 +136,7 @@
     
     self.loginUsernameTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"USER NAME" attributes:@{NSForegroundColorAttributeName: [UIColor lightGrayColor]}];
     self.loginPasswordTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"PASSWORD" attributes:@{NSForegroundColorAttributeName: [UIColor lightGrayColor]}];
-
+    
     
     NSDictionary *layoutViews = NSDictionaryOfVariableBindings(logo, username, password, loginButton, registerButton, moto);
     NSDictionary *layoutMetrics = @{@"textfieldWidth":[NSNumber numberWithInt:self.view.frame.size.width/2],
@@ -90,27 +167,6 @@
     
     [self.view addConstraints:horizontalConstraints];
     [self.view addConstraints:verticalConstraints];
-}
-
-#pragma mark - Login Methods
-- (IBAction)loginOnButtonPressed:(UIButton *)sender
-{
-    [self validateLoginCredentials];
-}
-
--(void)validateLoginCredentials
-{
-    //if error
-    
-    //else login
-    [self performSegueWithIdentifier:@"" sender:self];
-}
-
-#pragma mark - Resign Responders
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    [self.loginUsernameTextField resignFirstResponder];
-    [self.loginPasswordTextField resignFirstResponder];
 }
 
 @end
